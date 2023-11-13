a4bddebdc96df4dcd1695f0e92c452f0b0568083
Pouya <poesmaili@gmail.com>
2023-11-14 00:03:44 +0330

    Implement Square logic and passing the tests

--- src/main/java/edu/sharif/sel/Square.java
+++ src/main/java/edu/sharif/sel/Square.java
@@ -1,7 +1,22 @@
 package edu.sharif.sel;
 
 public class Square extends Polygon {
+    public Square(double length) {
+        sidesLengths = new double[2];
+        sidesLengths[0] = length;
+        sidesLengths[1] = length;
+    }
+
     public double computeArea() {
-        return 0.0;
+        return sidesLengths[0] * sidesLengths[1];
+    }
+
+    public double getLength() {
+        return sidesLengths[0];
+    }
+
+    public void setLength(double length) {
+        sidesLengths[0] = length;
+        sidesLengths[1] = length;
     }
 }
